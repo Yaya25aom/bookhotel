83b5e0b0621065c568371d7f00b701bf7e2a9912
Yaya25aom <99474159+Yaya25aom@users.noreply.github.com>
2023-02-20 18:06:36 +0700

    Add files via upload

--- hotel.sql
+++ hotel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2021 at 05:30 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.4.18
+-- Generation Time: Feb 20, 2023 at 12:03 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,21 +26,8 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `image_room`
 --
-
-CREATE TABLE `image_room` (
-  `id_image` int(11) NOT NULL,
-  `name_image` varchar(255) DEFAULT NULL,
-  `id_room` int(11) DEFAULT NULL,
-  `url` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `image_room`
---
-
-INSERT INTO `image_room` (`id_image`, `name_image`, `id_room`, `url`) VALUES
-(1, 'TEST', 101, 'https://s3.amazonaws.com/xcaliber/images/649/o/9f1cf64f-619c-4a22-ad9f-2ad8c2611549_Grand_Superior_210616_13.jpg'),
-(2, 'TEST2', 101, 'https://s3.amazonaws.com/xcaliber/images/649/o/156417ab-b22e-459e-a527-60b7fbe7f17f_Grand_Superior_210616_14.jpg');
+-- Error reading structure for table hotel.image_room: #1932 - Table 'hotel.image_room' doesn't exist in engine
+-- Error reading data for table hotel.image_room: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hotel`.`image_room`' at line 1
 
 -- --------------------------------------------------------
 
@@ -55,7 +42,6 @@ CREATE TABLE `member` (
   `email` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `phonenumber` int(11) NOT NULL,
   `status` enum('0','1') NOT NULL DEFAULT '1',
   `role` varchar(20) NOT NULL DEFAULT 'user'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
@@ -64,96 +50,49 @@ CREATE TABLE `member` (
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`UserID`, `firstname`, `lastname`, `email`, `username`, `password`, `phonenumber`, `status`, `role`) VALUES
-(010, 'sirinya', 'ausha', 'sirinyaannusah46@gmail.com', 'test', '81dc9bdb52d04dc20036dbd8313ed055', 0, '1', 'user'),
-(011, 'Admin', 'Bobo', '', 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 0, '1', 'admin');
+INSERT INTO `member` (`UserID`, `firstname`, `lastname`, `email`, `username`, `password`, `status`, `role`) VALUES
+(010, 'sirinya', 'ausha', 'sirinyaannusah46@gmail.com', 'test', 'sirin', '1', 'user'),
+(011, 'Admin', 'Bobo', '', 'admin', '81dc9bdb52d04dc20036dbd8313ed055', '1', 'admin'),
+(012, 'yani', 'aomsin', 'noinnnnn@gmail.com', 'nuy', '12345', '1', 'user'),
+(013, 'yanisa', 'aom', 'nnnnnn@gmail.com', 'noo', '1234', '1', 'user'),
+(014, 'ya', 'ao', 'saisamorn.ainfinity@gmail.com', 'na', '81dc9bdb52d04dc20036dbd8313ed055', '1', 'user'),
+(015, 'nuuuu', 'aommm', 'nuy@gmail.com', 'gom', 'e10adc3949ba59abbe56e057f20f883e', '1', 'user'),
+(016, 'are', 'you', 'noinangnuy@hotmail.com', 'nmo', 'b59c67bf196a4758191e42f76670ceba', '1', 'user'),
+(017, 'bbbb', 'aaaa', 'nnniuu@gmail.com', 'nan', '81dc9bdb52d04dc20036dbd8313ed055', '1', 'user'),
+(018, 'anuchit', 'pokpok', 'nicnic@gmail.com', 'nictest', '827ccb0eea8a706c4c34a16891f84e7b', '1', 'user'),
+(019, 'runya', 'aimaem', 'runya46@gmail.com', 'bob', '81dc9bdb52d04dc20036dbd8313ed055', '1', 'user'),
+(020, 'sasina', 'aomsin', 'nnopniuu@gmail.com', 'sisa', '81dc9bdb52d04dc20036dbd8313ed055', '1', 'user'),
+(021, 'nuuuuuu', 'oooooo', 'nnnjjjjiuu@gmail.com', 'yanirin', '827ccb0eea8a706c4c34a16891f84e7b', '1', 'user'),
+(022, 'sssssss', 'sssssssss', 'sssssss@email.com', 'ya', '827ccb0eea8a706c4c34a16891f84e7b', '1', 'user'),
+(023, 'purmpoon', 'llllll', 'mmm@email.com', 'pp', '81dc9bdb52d04dc20036dbd8313ed055', '1', 'user');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `order_room`
 --
-
-CREATE TABLE `order_room` (
-  `id_order` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `member_id` int(11) NOT NULL,
-  `datestart` date NOT NULL,
-  `dateend` date NOT NULL,
-  `price` int(11) NOT NULL,
-  `payment_status` enum('0','1','2') NOT NULL DEFAULT '0',
-  `status` enum('0','1') NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `order_room`
---
-
-INSERT INTO `order_room` (`id_order`, `room_id`, `member_id`, `datestart`, `dateend`, `price`, `payment_status`, `status`) VALUES
-(11, 101, 10, '2021-10-13', '2021-10-16', 8100, '0', '1'),
-(13, 101, 10, '2021-10-14', '2021-10-15', 200, '0', '1');
+-- Error reading structure for table hotel.order_room: #1932 - Table 'hotel.order_room' doesn't exist in engine
+-- Error reading data for table hotel.order_room: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hotel`.`order_room`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `room`
 --
-
-CREATE TABLE `room` (
-  `id_room` int(11) NOT NULL,
-  `type_room` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `status` enum('on','off') NOT NULL DEFAULT 'on',
-  `size` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `room`
---
-
-INSERT INTO `room` (`id_room`, `type_room`, `price`, `status`, `size`) VALUES
-(101, 1, 2700, 'off', 18),
-(102, 1, 3000, 'off', 20),
-(103, 2, 2000, 'on', 16);
+-- Error reading structure for table hotel.room: #1932 - Table 'hotel.room' doesn't exist in engine
+-- Error reading data for table hotel.room: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hotel`.`room`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `type_room`
 --
-
-CREATE TABLE `type_room` (
-  `id_type` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `type_bed` varchar(100) NOT NULL,
-  `size_start` int(10) NOT NULL,
-  `wifi` enum('yes','no') NOT NULL DEFAULT 'yes',
-  `people` int(11) NOT NULL,
-  `price_start` int(11) NOT NULL,
-  `status` enum('on','off') NOT NULL DEFAULT 'on',
-  `head_pic` text NOT NULL,
-  `buttom_pic` text NOT NULL,
-  `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `type_room`
---
-
-INSERT INTO `type_room` (`id_type`, `name`, `type_bed`, `size_start`, `wifi`, `people`, `price_start`, `status`, `head_pic`, `buttom_pic`, `text`) VALUES
-(1, 'Grand Superior', 'Double Bed', 18, 'yes', 2, 27000, 'on', 'https://images.mrandmrssmith.com/images/1736x1302/4705779-soori-bali-bali-indonesia.jpg', 'https://images.mrandmrssmith.com/images/1736x1302/4705779-soori-bali-bali-indonesia.jpg', 'Grand Superior rooms offer the perfect place to unwind and enjoy a pleasant night’s sleep.'),
-(2, 'Standard Room', 'Double Bed', 16, 'yes', 0, 2001, 'on', 'abc.jpg', 'https://images.mrandmrssmith.com/images/1736x1302/4705779-soori-bali-bali-indonesia.jpg', 'modern room with amenities for a peaceful stay.'),
-(3, 'ads', 'das', 255, 'yes', 2, 1500, 'on', 'images/32295.png', 'images/iMac_24-in_Orange_4-port_PDP_Image_Position-1__TH.png', 'asdsad');
+-- Error reading structure for table hotel.type_room: #1932 - Table 'hotel.type_room' doesn't exist in engine
+-- Error reading data for table hotel.type_room: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hotel`.`type_room`' at line 1
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `image_room`
---
-ALTER TABLE `image_room`
-  ADD PRIMARY KEY (`id_image`);
 
 --
 -- Indexes for table `member`
@@ -163,56 +102,14 @@ ALTER TABLE `member`
   ADD UNIQUE KEY `Username` (`firstname`);
 
 --
--- Indexes for table `order_room`
---
-ALTER TABLE `order_room`
-  ADD PRIMARY KEY (`id_order`);
-
---
--- Indexes for table `room`
---
-ALTER TABLE `room`
-  ADD PRIMARY KEY (`id_room`);
-
---
--- Indexes for table `type_room`
---
-ALTER TABLE `type_room`
-  ADD PRIMARY KEY (`id_type`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `image_room`
---
-ALTER TABLE `image_room`
-  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `UserID` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `order_room`
---
-ALTER TABLE `order_room`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `room`
---
-ALTER TABLE `room`
-  MODIFY `id_room` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1012;
-
---
--- AUTO_INCREMENT for table `type_room`
---
-ALTER TABLE `type_room`
-  MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `UserID` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
